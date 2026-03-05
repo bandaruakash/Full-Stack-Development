04e78e5bf83ebf86e6825c574ff1a71d9a6a55df
bandaruakash <bandaruakashb@gmail.com>
2026-03-05 18:25:54 +0530

    Add files via upload

---
+++ student/student.sql
@@ -0,0 +1,8 @@
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    dob DATE,
+    department VARCHAR(50),
+    phone VARCHAR(15)
+);
